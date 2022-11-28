3b85cf026514819075a186096a9608b61be10cc3
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2022-11-29 01:51:09 +0530

    Time: 264 ms (29.78%), Space: 19.8 MB (95.26%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
         
-        for(int i=0; i<nums.size()-2; i++){
-            while(i!=0 && i<nums.size()-2 && nums[i]==nums[i-1])
-                i++;
-            
-            int l = i+1, r = nums.size()-1;
-            while(l<r) {
-                if(nums[l]+nums[r] == -nums[i]) {
-                    ans.push_back({nums[i], nums[l], nums[r]});
-                    while(l<r && nums[l]==nums[l+1])
+        for(int i=0; i<nums.size()-2; i++) {
+            if(i==0 || nums[i]!=nums[i-1]) {
+                int l = i+1, r = nums.size()-1;
+                while(l<r) {
+                    if(nums[l] + nums[r] == -nums[i]) {
+                        ans.push_back({nums[i], nums[l], nums[r]});
+                        while(l<r && nums[l]==nums[l+1])
+                            l++;
+                        while(l<r && nums[r]==nums[r-1])
+                            r--;
                         l++;
-                    while(l<r && nums[r]==nums[r-1])
                         r--;
-                    l++;
-                    r--;
+                    }
+                    else if(nums[l] + nums[r] < -nums[i])
+                        l++;
+                    else 
+                        r--;
                 }
-                else if(nums[l] + nums[r] < -nums[i]) {
-                    l++;
-                }
-                else 
-                    r--;
             }
         }
         return ans;
