723a6c9537fc9191808d43110397e55f2b83e0b4
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2022-12-13 13:41:50 +0530

    Time: 4 ms (42.08%), Space: 6.2 MB (98.79%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.size() > t.size())
+            return false;
+        
+        int i=0, j=0;
+        while(j<t.size()) {
+            if(s[i]==t[j]) {
+                i++;
+            }
+            j++;
+        }
+        
+        return i==s.size();
+    }
+};
