5332646b09080009bbf1ae9178504073b6bf7ed7
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2022-12-14 23:51:01 +0530

    Time: 3 ms (76.44%), Space: 7.1 MB (70.11%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void findSubsets(vector<int>& nums, vector<int> curr, vector<vector<int>> &allSubsets, int i) {
-        if(i>=nums.size()) {
-            allSubsets.push_back(curr);
-            return;
-        }
-        curr.push_back(nums[i]);
-        findSubsets(nums, curr, allSubsets, i+1);
-        curr.pop_back();
-        findSubsets(nums, curr, allSubsets, i+1);
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> allSubsets;
-        vector<int> curr;
-        findSubsets(nums, curr, allSubsets, 0);
+        int n = 1 << nums.size();
+        for(int i=0; i<n; i++) {
+            vector<int> currSet;
+            int pos = 1;
+            for(int j=0; j<nums.size(); j++) {
+                pos = pos << j;
+                if(pos & i) {
+                    currSet.push_back(nums[j]);
+                }
+                pos = 1;
+            }
+            allSubsets.push_back(currSet);
+        }
         return allSubsets;
     }
 };
