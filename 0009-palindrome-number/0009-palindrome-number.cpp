d726353b4a4af76e2f590764d5d989c6ed1147ee
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2025-06-04 20:04:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -7,8 +7,6 @@ public:
         int n = x;
         int reverse = 0;
         while(n > 0) {
-            if(reverse > INT_MAX / 10)
-                return false;
             reverse = reverse * 10 + n % 10;
             n /= 10;
         }
